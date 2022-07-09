6af9810f23847c892d8752c0bfcff7f0f1d84ddb
Jennie <jenniebuitrago@gmail.com>
2022-07-08 21:40:05 -0300

    Deploy

---
+++ cliente/docs/static/js/main.2b4c4c31.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpclontagram-cliente"]=this["webpackJsonpclontagram-cliente"]||[]).push([[0],{10:function(n,e,t){"use strict";t.r(e);t(0);var c=t(3),o=t.n(c),r=(t(8),t(1));function i(){return Object(r.jsx)("h1",{children:"\xa1Bienvenido al curso!"})}o.a.render(Object(r.jsx)(i,{}),document.getElementById("root"))},8:function(n,e,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.2b4c4c31.chunk.js.map
